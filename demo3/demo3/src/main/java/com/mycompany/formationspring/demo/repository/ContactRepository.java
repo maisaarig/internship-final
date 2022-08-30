e277d0ed21f90dcc6e94bd6e40c2eb3eda854d1d
Maisaa <93721514+maisaarig@users.noreply.github.com>
2022-08-30 18:18:34 +0200

    Delete demo3/demo3 directory

--- demo3/demo3/src/main/java/com/mycompany/formationspring/demo/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.formationspring.demo.repository;
-
-import com.mycompany.formationspring.demo.entity.Contact;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContactRepository extends CrudRepository<Contact, Long> {
-}
--- demo3/demo3/target/demo-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO CONTACT (FIRST_NAME,LAST_NAME) VALUES
-  ('Jean-Claude','Goldman'),
-  ('John','Volt'),
-  ('Kevina','Blum');
